b2d37c38497d25b51ca5b823ea20c0da3afea131
Igor Gaponov <jiminy96@gmail.com>
2015-04-16 06:54:02 +0200

    upd Error test with serialization

--- tests/Spec/ErrorTest.php
+++ tests/Spec/ErrorTest.php
@@ -9,8 +9,10 @@ class ErrorTest extends \PHPUnit_Framework_TestCase
         $code = 0;
         $message = 'err';
         $data = ['data'];
-        $error = new Error($code, $message, $data);
-        $serialized = $error->jsonSerialize();
-        $this->assertEquals(compact('code', 'message', 'data'), $serialized);
+        $serialized = json_encode(new Error($code, $message, $data));
+        $error = json_decode($serialized);
+        $this->assertAttributeEquals($code, 'code', $error);
+        $this->assertAttributeEquals($message, 'message', $error);
+        $this->assertAttributeEquals($data, 'data', $error);
     }
 }
