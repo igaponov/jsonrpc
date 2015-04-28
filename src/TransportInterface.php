19e664f7cb98c2c926b13e881095d392b3e189c6
Igor Gaponov <jiminy96@gmail.com>
2015-04-28 05:57:47 +0200

    added phpdoc for Transport::send()

--- src/TransportInterface.php
+++ src/TransportInterface.php
@@ -2,9 +2,18 @@
 
 namespace JsonRpc;
 
+use JsonRpc\Spec\BatchResponse;
+use JsonRpc\Spec\Response;
 use JsonRpc\Spec\UnitInterface;
 
 interface TransportInterface
 {
+    /**
+     * Send data to transport layer
+     *
+     * @param UnitInterface $data
+     * @param array $options
+     * @return Response|BatchResponse
+     */
     public function send(UnitInterface $data, $options = []);
 }
